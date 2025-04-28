1522c63da5f3542a67933c27a968e6503e04a2d4
Handy Claude Marie Milliance <milliance23@gmail.com>
2025-04-28 15:07:39 -0300

    atualizado

--- pages/game.tsx
+++ pages/game.tsx
@@ -1089,12 +1089,7 @@ export default function Game() {
                     {player.name}
                   </span>
                 </div>
-                {/**{hiddenPlayers.includes(player.clientId) && (
-                  <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 text-white ml-2" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M15 12a3 3 0 11-6 0 3 3 0 016 0z" />
-                    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M2.458 12C3.732 7.943 7.523 5 12 5c4.478 0 8.268 2.943 9.542 7-1.274 4.057-5.064 7-9.542 7-4.477 0-8.268-2.943-9.542-7z" />
-                  </svg>
-                )}**/}
+                
               </li>
             ))}
           </ul>
@@ -1213,8 +1208,8 @@ export default function Game() {
               rounded-bl-none rounded-br-none
               animate__faster 
             `}*/
-            className='{fixed bottom-0 left-1/2 -translate-x-1/2 z-50
-              w-full max-w-[calc(100vw-16px)] sm:max-w-md
+            className='{fixed bottom-6 right-6 z-50
+              w-full max-w-[calc(90vw-16px)] sm:max-w-md
               flex flex-col
               bg-gradient-to-br from-[#1e293b] via-[#334155] to-[#0f172a]
               border-t-4 border-cyan-500
