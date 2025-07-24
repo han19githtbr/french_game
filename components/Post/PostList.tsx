d5cf762d6aa2e4fcb6e33148a2ac8a1767d46679
Handy Claude Marie Milliance <milliance23@gmail.com>
2025-07-23 21:55:14 -0300

    atualizado

--- components/Post/PostList.tsx
+++ components/Post/PostList.tsx
@@ -10,7 +10,7 @@ export default function PostList() {
   const [filterTheme, setFilterTheme] = useState('');
   const [postsToShow, setPostsToShow] = useState<number>(2);
 
-  const themes = ['Gramática', 'Cultura', 'Gastronomia', 'Tecnologia', 'Ditados', 'Natureza', 'Turismo'];
+  const themes = ['Gramática', 'Cultura', 'Gastronomia', 'Tecnologia', 'Ditados', 'Natureza', 'Turismo', 'Pensamentos'];
 
   useEffect(() => {
     const fetchPosts = async () => {
