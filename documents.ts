4cfe88cf28df7c7556b74ef1ab7ae87bcc01813d
Handy Claude Marie Milliance <milliance23@gmail.com>
2025-10-08 14:26:40 -0300

    atualizado

---
+++ documents.ts
@@ -0,0 +1,57 @@
+[
+  {
+    "theme": "tecnologia",
+    "url": "/frases/tecnologia/painel.png",
+    "title": "Tableau de bord."
+  },
+  {
+    "theme": "tecnologia",
+    "url": "/frases/tecnologia/volante.png",
+    "title": "Volant."
+  },
+  {
+    "theme": "tecnologia",
+    "url": "/frases/tecnologia/retrovisor.png",
+    "title": "Rétroviseur."
+  },
+  {
+    "theme": "tecnologia",
+    "url": "/frases/tecnologia/caminhao.png",
+    "title": "Camion."
+  },
+  {
+    "theme": "tecnologia",
+    "url": "/frases/tecnologia/motor.png",
+    "title": "Moteur."
+  },
+  {
+    "theme": "gastronomia",
+    "url": "/frases/gastronomia/bolo_chocolate.png",
+    "title": "Gâteau au chocolat."
+  },
+  {
+    "theme": "gastronomia",
+    "url": "/frases/gastronomia/banana_madura.png",
+    "title": "Une figue banane"
+  },
+  {
+    "theme": "gastronomia",
+    "url": "/frases/gastronomia/banana.png",
+    "title": "Une banane."
+  },
+  {
+    "theme": "natureza",
+    "url": "/frases/natureza/flores.png",
+    "title": "Des fleurs"
+  },
+  {
+    "theme": "natureza",
+    "url": "/frases/natureza/coco.png",
+    "title": "Un cocotier"
+  },
+  {
+    "theme": "família",
+    "url": "/frases/família/casamento.png",
+    "title": "Un mariage"
+  }
+]
